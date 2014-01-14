6ceae28b91b4523fb92816811acb4b920101015b
Lampei@gmail.com <Lampei@gmail.com>
2014-01-14 14:04:25 -0500

    added ci directory for CI integration prep

---
+++ test/ci/scripts/ci-helper-railo.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+case $1 in
+	start)
+		CONTROL_SCRIPT='railo/start'
+		;;
+	stop)
+		CONTROL_SCRIPT='railo/stop'
+		;;
+esac
+
+MY_DIR=`dirname $0`
+source $MY_DIR/ci-helper-base.sh $1 $2
+
+case $1 in
+	install)
+		mv railo-express* railo
+		mv mxunit* railo/webapps/www/mxunit
+
+		ln -s $BUILD_DIR railo/webapps/www/$2
+
+		sed -i "s/jetty.port=8888/jetty.port=$SERVER_PORT/g" railo/start
+		sed -i "s/STOP.PORT=8887/STOP.PORT=$STOP_PORT/g" railo/start
+		sed -i "s/STOP.PORT=8887/STOP.PORT=$STOP_PORT/g" railo/stop
+		;;
+	start|stop)
+		;;
+	*)
+		echo "Usage: $0 {install|start|stop}"
+		exit 1
+		;;
+esac
+
+exit 0
